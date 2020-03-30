b7eb06dfcb30c228d1848ac69003923bc14681a7
Rolandas <rolandaswb@gmail.com>
2020-03-30 14:58:32 +0300

    test home page using django client

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,14 +6,17 @@ from django.http import HttpRequest
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_page_(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        # built in client usage
+        response = self.client.get('/')
+
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
+
+        # check for template usage
