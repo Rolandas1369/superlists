f037bb7d7f67dde40c31b0991b41a3e0a9d6d6a2
Rolandas <rolandaswb@gmail.com>
2020-03-31 17:40:44 +0300

    functional_test

--- functional_test.py
+++ functional_test.py
@@ -14,6 +14,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # add helping method for list testing
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn(row_text, rows.text)
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -38,14 +44,17 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+        # if test does have low time it may not catch values after enter
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,29 +1,25 @@
-from django.urls import resolve
 from django.test import TestCase
-from .views import home_page
-from django.http import HttpRequest
-import pprint
-# Create your tests here.
+
 class HomePageTest(TestCase):
 
     def test_uses_home_page_(self):
+        # built in client usage
         response = self.client.get('/')
+        # django built assert
         self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
-        # built in client usage
         response = self.client.get('/')
-
+        # readable format
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
 
-    # this way we are posting to / data={'item_text': 'A new list item'}
+    # this way we are posting to '/' path data={'item_text': 'A new list item'}
     def test_can_save_POST_request(self):
-
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        print(str(response.content.decode()))
+
         # checks if 'A new list item' in response
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
